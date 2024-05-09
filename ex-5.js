d2c56ba776af48a376df2fda322ed733b1bdaf0a
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-05-09 14:08:40 +0700

    finish the exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalProductValue = 27000;
+let discount = 1 / 4;
+
+let totalvalue = totalProductValue - totalProductValue * discount;
+console.log(totalvalue);
