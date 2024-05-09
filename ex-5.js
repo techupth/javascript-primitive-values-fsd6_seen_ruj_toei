00fc52b9c795495684e15591fb52ef5ed8ac7785
S33N <145544074+rizzboislw@users.noreply.github.com>
2024-05-09 13:49:59 +0700

    exercise 1-8 finished

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,11 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalProductValue = 27000;
+let discount = 3 / 4;
+
+let totalValue = totalProductValue - (totalProductValue * discount);
+console.log(totalValue);
+
+
